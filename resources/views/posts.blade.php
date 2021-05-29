90f39004587aa201937f4fbadec623ca8ee0d0fb
joseph-rodney <kaneenejoseph@gmail.com>
2021-05-29 19:41:42 +0300

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+
+    <link rel="stylesheet" href="/app.css"></link>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-fist-post"> First Blog</a></h1>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi mollitia repudiandae et aspernatur reiciendis, ullam autem nam fugiat odit delectus nesciunt rem dolor? Assumenda recusandae odio totam voluptate dignissimos perspiciatis?</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post"> Second Blog</a></h1>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi mollitia repudiandae et aspernatur reiciendis, ullam autem nam fugiat odit delectus nesciunt rem dolor? Assumenda recusandae odio totam voluptate dignissimos perspiciatis?</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post"> Third Blog</a></h1>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi mollitia repudiandae et aspernatur reiciendis, ullam autem nam fugiat odit delectus nesciunt rem dolor? Assumenda recusandae odio totam voluptate dignissimos perspiciatis?</p>
+    </article>
+    
+</body>
+</html>
