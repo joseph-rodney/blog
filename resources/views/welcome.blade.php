2c7489a565934af03b358ecbc1f59368e652f094
joseph-rodney <kaneenejoseph@gmail.com>
2021-05-30 23:05:51 +0300

    Added markup to welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css"/>
+
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container{
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content{
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title{
+            font-size: 96px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Welcome</div>
+        </div>
+    </div>
+</body>
+</html>
