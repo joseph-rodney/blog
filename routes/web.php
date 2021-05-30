d43137cd27ebfc6ba6a67f53eaffac757a2f29b6
joseph-rodney <kaneenejoseph@gmail.com>
2021-05-30 22:51:08 +0300

    Unrooted post.blade.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('', function () {
     $posts = Post::all();
     return view('posts', [
         'posts' => Post::all()
