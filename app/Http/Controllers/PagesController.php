027fa50db1e531b52b52056e0eb01f4388d87f91
joseph-rodney <kaneenejoseph@gmail.com>
2021-05-31 20:21:30 +0300

    Added PagesController

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller
+{
+    public function getIndex()
+    {
+        return view(
+            'pages.welcome'
+        );
+    }
+    public function getAbout()
+    {
+        $first = 'Rodney';
+        $last = 'Kaneene';
+        $full = $first . " " . $last;
+        return view(
+            'pages.about'
+        )->with("fullname", $full);
+    }
+    public function getContact()
+    {
+        return view(
+            'pages.contact'
+        );
+    }
+}
