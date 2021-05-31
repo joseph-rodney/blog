6518e0eced54f56067b4f8deeebc62b76bdbfdc0
joseph-rodney <kaneenejoseph@gmail.com>
2021-05-31 07:45:29 +0300

    Removed style tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,32 +17,7 @@
 
     <!--app.css-->
     <link rel="stylesheet" href="../public/app.css" type="text/css"/>
-
-    <style>
-        body{
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container{
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content{
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title{
-            font-size: 96px;
-        }
-    </style>
+    
 </head>
 <body>
     <div class="container">
