b9ec5d18f3cfdcfb245026820fae1225e458cd76
joseph-rodney <kaneenejoseph@gmail.com>
2021-05-31 19:54:30 +0300

    Added markup to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
                     <i class="fas fa-bars"></i>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item" href="/welcome">Home</a></li>
+                    <li><a class="dropdown-item" href="#welcome">Home</a></li>
                     <li><a class="dropdown-item" href="/about">About</a></li>
                     <li><a class="dropdown-item" href="/contact">Contact</a></li>
                 </ul>
@@ -37,12 +37,47 @@
         </div>
     </nav>
     <!--Navbar end-->
+
+    <!--Jumbotron section start-->
     <div class="container">
-        <div class="content">
-            <div class="title">Welcome</div>
-            <p> Welcome Everyone!</p>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron mt-5 pt-5">
+                    <h1>Welcome to my Blog</h1>
+                    <p class="lead">Thank you for visiting. This is my test Website built with Laravel. Please read my Latest post.</p>
+                    <a href="#" class="btn btn-danger btn-lg" role="button">Popular Post</a>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-8">
+                <div class="post">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ipsa repellendus laboriosam natus commodi temporibus, reprehenderit nihil nemo labore numquam unde, impedit, voluptatem quaerat. Cumque doloremque quis deleniti dolorem fugiat.</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+                <div class="post mt-3">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ipsa repellendus laboriosam natus commodi temporibus, reprehenderit nihil nemo labore numquam unde, impedit, voluptatem quaerat. Cumque doloremque quis deleniti dolorem fugiat.</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+                <div class="post mt-3">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ipsa repellendus laboriosam natus commodi temporibus, reprehenderit nihil nemo labore numquam unde, impedit, voluptatem quaerat. Cumque doloremque quis deleniti dolorem fugiat.</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+                <div class="post mt-3">
+                    <h3>Post Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ipsa repellendus laboriosam natus commodi temporibus, reprehenderit nihil nemo labore numquam unde, impedit, voluptatem quaerat. Cumque doloremque quis deleniti dolorem fugiat.</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+            <div class="col-md-3 col-md-offset-1">
+                <h3>Side bar</h3>
+            </div>
         </div>
     </div>
+    <!--Jumotron section end-->
 
   <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
